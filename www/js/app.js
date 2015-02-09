38c239f3505e8054baea818d1e5e94cb8e526646
Luisita <luiyi_1414@hotmail.com>
2015-02-09 01:48:06 -0600

    1

--- www/js/app.js
+++ www/js/app.js
@@ -15,13 +15,13 @@ window.onload = function(){
    var ctx = cargaContextoCanvas('micanvas');
    if(ctx){
       //Creo una imagen conun objeto Image de Javascript
-      var img = new Image();
-      //indico la URL de la imagen
-      img.src = 'img/logo.png';
+      var imgList = new Array();
+      imgList.push(new Image());
+      imgList[0].src="img/fresa.jpg";
       //defino el evento onload del objeto imagen
       img.onload = function(){
          //incluyo la imagen en el canvas
-         ctx.drawImage(img, 10, 10);
+         ctx.drawImage(imgList[0], 10, 10);
       }
    }
 }
